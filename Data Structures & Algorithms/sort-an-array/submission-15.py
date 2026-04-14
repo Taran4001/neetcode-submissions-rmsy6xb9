8519b9ec55a117843071ef202faad8a4573b8349
Taran4001 <99532519+Taran4001@users.noreply.github.com>
2026-04-14 21:00:13 +0530

    Add: sort-an-array - submission-15

---
+++ Data Structures & Algorithms/sort-an-array/submission-15.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            temp = nums[i]
+            j = i - 1
+            while j >= 0 and nums[j] > temp:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = temp
+        return nums
