5fb0653eea135d135ff049cef0c574567580f9e4
Taran4001 <99532519+Taran4001@users.noreply.github.com>
2026-07-12 13:44:08 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s = list()
+        for string in strs:
+            s.append(str(len(string)) + "#" + string)
+        return "".join(s)
+
+    def decode(self, s: str) -> List[str]:
+        strs = list()
+        i = 0
+        while i < len(s):
+            j = i
+            strLen = list()
+            while s[j].isdigit():
+                strLen.append(s[j])
+                j += 1
+            strs.append(s[j + 1:int("".join(strLen)) + j + 1])
+            i = int("".join(strLen)) + j + 1
+
+
+        return strs
